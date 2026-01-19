f28b7380c2f9fde3101102954ae5105f69ed0904
Vadim Pichurin <vadimpich@gmail.com>
2026-01-19 23:45:09 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/studio-potato/sw.js', { scope: '/studio-potato/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
